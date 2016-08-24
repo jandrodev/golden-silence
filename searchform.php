047dda932023bdbda317fdf2d32c200cea3e5aa0
jandro935 <jandro935@gmail.com>
2016-08-24 17:01:00 +0200

    Update searchform.php

--- searchform.php
+++ searchform.php
@@ -1,12 +1,3 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: alejandro.seisdedos
- * Date: 18/05/2016
- * Time: 12:03
- */
-?>
-
 <div>
     <form role="form" action="/">
         <input type="text" name="s" id="search" value="<?php the_search_query(); ?>" autocomplete="off" placeholder="Search">
